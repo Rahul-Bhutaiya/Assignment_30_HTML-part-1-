358b7a2cb8ab138ded8bb657ee6a4334e52b4245
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-12-13 09:30:14 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,5 @@
+var1,var2=int(input('enter first value : ')),int(input('enter second value : '))
+print('var1 :%d \nvar2 :%d'%(var1,var2))
+
+var1,var2=var2,var1
+print('After Swapping Values Are :\nvar1 :%d \nvar2 :%d'%(var1,var2))
